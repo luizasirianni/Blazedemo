3d2e65f86f0db333750400b6745daac1c536f641
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-04-04 18:59:54 +0100

    Aumentado o tempo de implicitly wait

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 def before_all(context):
     context.driver = webdriver.Chrome()
-    context.driver.implicitly_wait(5)
+    context.driver.implicitly_wait(15)
     context.driver.maximize_window()
 
     print('Passo A - antes de tudo')
