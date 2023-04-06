c789ee4db86965987b939be22e116696c1e783d6
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-04-06 18:20:17 +0100

    Criado passo a passo do scenario 'Login com sucesso'

--- features/steps/Login.py
+++ features/steps/Login.py
@@ -6,14 +6,22 @@ from selenium.webdriver.support.select import Select
 
 @when(u'clico em "Home"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When clico em "Home"')
+    context.driver.find_element(By.CSS_SELECTOR, 'body:nth-child(2) div.navbar.navbar-inverse:nth-child(1) '
+                                                 'div.navbar-inner div.container > a.brand:nth-child(3)').click()
+    print('Passo 2 - Acessou a pagina Home')
 
 
 @when(u'preencho "<email>" e "<senha>" e clico no botao "Login"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When preencho "<email>" e "<senha>" e clico no botao "Login"')
-
+    context.driver.find_element(By.ID, 'email').send_keys('emailexemplo@hotmail.com')
+    context.driver.find_element(By.ID, 'password').send_keys('123455')
+    context.driver.find_element(By.CSS_SELECTOR, 'div.container div.row div.col-md-8.col-md-offset-2 '
+                                                 'div.panel.panel-default div.panel-body form.form-horizontal '
+                                                 'div.form-group:nth-child(5) div.col-md-8.col-md-offset-4 > '
+                                                 'button.btn.btn-primary:nth-child(1)').click()
+    print('Passo 3 - Preencheu os dados e clicou em Login')
 
 @then(u'vejo a mensagem de confirmacao')
+#o site atualmente está com erro de Page Expired
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then vejo a mensagem de confirmacao')
+    assert context.driver.find_element(By.XPATH, "//div[contains(text(),'Page Expired')]")
