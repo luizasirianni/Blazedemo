013a1bf5383aa562f517c2f78cabedda21ee21c5
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-04-10 21:19:09 +0100

    Adicionado passo para verificar a imagem contida na página

--- features/steps/DestinoDaSemana.py
+++ features/steps/DestinoDaSemana.py
@@ -1,5 +1,5 @@
 from behave import when, then
-from selenium import *
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 @when(u'clico em "check out our destination of the week"')
 def step_impl(context):
@@ -9,4 +9,6 @@ def step_impl(context):
 @then(u'visualizo o destino da semana')
 def step_impl(context):
     assert context.driver.find_element(By.XPATH, '//body/div[2]').text == 'Destination of the week: Hawaii !'
-    assert context.driver.find_element(By.XPATH, '//body/div[2]/img[1]')
+    print('Passo 3.1: Visualizo o nome do destino')
+    assert context.driver.find_element(By.XPATH, '/html/body/div[2]/img').get_attribute('src') == 'https://www.blazedemo.com/assets/vacation.jpg'
+    print('Passo 3.2: Visualizo a imagem do destino')
